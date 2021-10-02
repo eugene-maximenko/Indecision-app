51c33d4142083263b60a967809a9693aa81623f1
tiger3120 <zhek4770@gmail.com>
2021-10-03 01:10:48 +0300

    Configured the app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js if running!')
+
+// JSX - JavaScript XML
+var template = <h1>Does this change!?</h1>
+var appRoot = document.getElementById('app')
+
+
+ReactDOM.render(template, appRoot)
