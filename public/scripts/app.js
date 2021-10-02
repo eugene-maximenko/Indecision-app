0105b358e252eba0f8a35d5c4d6d28a8f2451866
tiger3120 <zhek4770@gmail.com>
2021-10-02 21:23:43 +0300

    Init commit

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+console.log('App.js if running!')
+
+// JSX - JavaScript XML
+// var template = <p>This is JSX from app.js!</p>
+var template = /*#__PURE__*/React.createElement("p", {
+    id: "someid"
+}, "This is JSX from app.js!");
+var appRoot = document.getElementById('app')
+
+
+ReactDOM.render(template, appRoot)
