74516eb8bd6992216236764309d68ed2a2ba072a
tiger3120 <zhek4770@gmail.com>
2021-10-03 15:17:02 +0300

    Say hello to dynamic React pages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,68 @@
 
 console.log('App.js if running!');
 
-// JSX - JavaScript XML
-var template = React.createElement(
-  'h1',
-  null,
-  'Does this change!?'
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer'
+
+    // JSX - JavaScript XML
+};var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var user = {
+    name: 'Eugene',
+    age: 22,
+    location: 'Philadelphia'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
