b6f675df18420b0ca9462d04444c46b30e156e85
tiger3120 <zhek4770@gmail.com>
2021-10-03 23:20:48 +0300

    Repeat difference between var let const

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,22 @@
+var nameVar = 'Eugene'
+var nameVar = 'Mike'
+console.log('nameVar', nameVar)
+
+let nameLet = 'Jen'
+nameLet = 'Julie'
+console.log('nameLet', nameLet)
+
+const nameConst = 'Frank'
+console.log('nameConst', nameConst)
+
+// Block scoping
+
+const  fullName = 'Jen Maximenko'
+let firstName
+
+if (fullName) {
+    firstName = fullName.split(' ')[0]
+    console.log(firstName);
+}
+
+console.log(firstName);
