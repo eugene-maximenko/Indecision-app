6f0b3e788f62b31511e79043b27f2780724e0c25
ShadowPrince <zhek4770@gmail.com>
2021-10-27 01:06:07 +0300

    Added 3rd party libraries

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-// // // import './utils'
-// import subtract, { square, add } from './utils'
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log('app.js is running! =)')
-// console.log(square(4))
-// console.log(add(100, 23))
-// console.log(subtract(100, 23))
-
-import isSenior, { isAdult, canDrink } from './person'
-console.log(isAdult(18))
-console.log(canDrink(18))
-console.log(isSenior(64))
-
+const template = React.createElement('p', {}, 'testing 123')
+ReactDOM.render(template, document.getElementById('app'))
