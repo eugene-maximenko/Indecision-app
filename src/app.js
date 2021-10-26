42cace9a512f6639f0d221f65c650f3616ffd739
ShadowPrince <zhek4770@gmail.com>
2021-10-27 00:22:01 +0300

    Add default export-import
    Just repeat export-import expressions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-// // import './utils'
-// import { square, add } from './utils'
+// // // import './utils'
+// import subtract, { square, add } from './utils'
 
 // console.log('app.js is running! =)')
 // console.log(square(4))
 // console.log(add(100, 23))
+// console.log(subtract(100, 23))
 
-import { isAdult, canDrink } from './person'
-
+import isSenior, { isAdult, canDrink } from './person'
 console.log(isAdult(18))
 console.log(canDrink(18))
+console.log(isSenior(64))
+
